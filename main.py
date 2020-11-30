7c65366549370dfda1452c5a92b3b7695f5eb49e
RazdolbayOne <Yone312@gmail.com>
2020-11-30 22:44:59 +0200

    raw version of functionality

--- main.py
+++ main.py
@@ -4,31 +4,46 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import random
 
+##here are stored chinese charecters and translates
+FILE_NAME ="words.txt"
 
-def memorizer(name_list, answer_list):
-    # Use a breakpoint in the code line below to debug your script.
-    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    word_length = (len(answer_list) - 1)
-    index = random.randint(0, word_length)
-    i = 0
-    while (i != word_length):
-        print("--------------")
-        print(name_list[index])
-        input()
-        print(answer_list[index])
-        print()
-        index = random.randint(0, word_length)
-        i += 1
-    print("--------------")
+
+def read_from_file(filename)->bool:
+    # TODO make what fuction reads in proper way words and stors then in dir
+    try:
+        with open(filename) as f:
+            print(f.read())
+            # Do something with the file
+    except IOError:
+        return False
+    return True
+
+def main():
+    # check if input file provided
+    if FILE_NAME =="":
+        # TODO better file detection
+        print("no file name provided,programme quite!!!")
+        return
+    # check if file can be opened and read it if so
+    if not read_from_file(FILE_NAME):
+        print("File \""+str(FILE_NAME)+"\" not readable, programme quite")
+        return
+
+    current_chose = ''
+
+    while True:
+        #TODO write what can user chose from
+        if current_chose == 'q':
+            break
+        current_chose=input()
+        print("to get out of press q key")
+        # TODO write part where shows up in console stuff
+        while True:
+            pass
+
+    return 0
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    names = "(女儿) (几) (岁) (了) (今年) (有) (家) 口"
-    answers = "daughter howMany yearOld newCircs thisYear toHave family measureWord"
-    name_list = names.split()
-    answers_list = answers.split()
-
-    memorizer(name_list, answers_list)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
